e617de0ab9d9ff6256fa3a93bdb7f56da75f29c7
李野 <liye@vidmt.com>
2018-01-04 10:39:24 +0800

    创建cat类

---
+++ weibo/weibo/cat.h
@@ -0,0 +1,13 @@
+//
+//  cat.h
+//  weibo
+//
+//  Created by LyforMac on 2018/1/4.
+//  Copyright © 2018年 playTeam. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cat : NSObject
+
+@end
---
+++ weibo/weibo/cat.m
@@ -0,0 +1,13 @@
+//
+//  cat.m
+//  weibo
+//
+//  Created by LyforMac on 2018/1/4.
+//  Copyright © 2018年 playTeam. All rights reserved.
+//
+
+#import "cat.h"
+
+@implementation cat
+
+@end

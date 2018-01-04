90198078f27bdd312cd1f3ae44a45400a6be7d6a
李野 <liye@vidmt.com>
2018-01-04 10:33:28 +0800

    初始化项目

---
+++ weibo/weibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weibo
+//
+//  Created by LyforMac on 2018/1/4.
+//  Copyright © 2018年 playTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ weibo/weibo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  weibo
+//
+//  Created by LyforMac on 2018/1/4.
+//  Copyright © 2018年 playTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
